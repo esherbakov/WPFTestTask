68ba670e72dd5f2cff9457ec032f50ceec5aa6af
esherbakov <drowsywh@gmail.com>
2024-09-12 17:14:49 +0700

    Добавьте файлы проекта.

---
+++ WPFTestTask/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPFTestTask
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
